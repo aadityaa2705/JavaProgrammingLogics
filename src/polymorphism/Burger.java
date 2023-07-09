9b98a6653b30c57a5d37d204c2c53c3ebcab2631
Aditya <adityabarathe100@gmail.com>
2023-07-09 12:35:05 +0530

    changed name from hamburger to burger to make it more general

--- src/polymorphism/Burger.java
+++ src/polymorphism/Burger.java
@@ -1,6 +1,6 @@
 package polymorphism;
 
-public class Hamburger {
+public class Burger {
     private String name;
     private String meat;
     private double price;
@@ -18,32 +18,32 @@ public class Hamburger {
     private String addition4Name;
     private double addition4Price;
 
-    public Hamburger(String name, String meat, double price, String breadRollType) {
+    public Burger(String name, String meat, double price, String breadRollType) {
         this.name = name;
         this.meat = meat;
         this.price = price;
         this.breadRollType = breadRollType;
     }
 
-    public void addHamburgerAddition1(String name,double price){
+    public void addBurgerAddition1(String name, double price){
         this.addition1Name=name;
         this.addition1Price=price;
     }
-    public void addHamburgerAddition2(String name,double price){
+    public void addBurgerAddition2(String name, double price){
         this.addition2Name=name;
         this.addition2Price=price;
     }
 
-    public void addHamburgerAddition3(String name,double price){
+    public void addBurgerAddition3(String name, double price){
         this.addition3Name=name;
         this.addition3Price=price;
     }
-    public void addHamburgerAddition4(String name,double price){
+    public void addBurgerAddition4(String name, double price){
         this.addition4Name=name;
         this.addition4Price=price;
     }
 
-    public double itemizeHamburger(){
+    public double itemizeBurger(){
         double totalPrice = this.price;
         System.out.println(this.name+ " burger on a "+this.breadRollType+" bread with "+this.meat+", price is "+this.price+" Rs");
         if (addition1Name!=null){
--- src/polymorphism/MainApp.java
+++ src/polymorphism/MainApp.java
@@ -2,11 +2,11 @@ package polymorphism;
 
 public class MainApp {
     public static void main(String[] args) {
-        Hamburger hamburger = new Hamburger("Jumbo","Chicken",65,"White Sesame");
-        hamburger.addHamburgerAddition2("Tomato",10);
-        hamburger.addHamburgerAddition2("Cheese",20);
-        hamburger.addHamburgerAddition3("Cola",25);
-        double totalBill = hamburger.itemizeHamburger();
+        Burger burger = new Burger("Jumbo","Chicken",65,"White Sesame");
+        burger.addBurgerAddition2("Tomato",10);
+        burger.addBurgerAddition2("Cheese",20);
+        burger.addBurgerAddition3("Cola",25);
+        double totalBill = burger.itemizeBurger();
         System.out.println("Total Bill : "+totalBill+" Rs");
     }
 }
